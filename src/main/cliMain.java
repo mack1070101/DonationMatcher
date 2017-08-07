ca9e84105a976252410f5fe782db10caf529222c
mackenzie <mackenziebligh@gmail.com>
2017-08-07 15:18:15 -0600

    Update project with cliMain

---
+++ src/main/cliMain.java
@@ -0,0 +1,10 @@
+package models;
+
+/**
+ * Created by mackenzie on 07/08/17.
+ */
+public class cliMain {
+    public static void main(String args[]){
+        System.out.println("Hello world"); // Holder for main
+    }
+}
