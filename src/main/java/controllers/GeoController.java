55a4013771897e0f71aaa9346603f400a4e2871d
mackenzie <mackenziebligh@gmail.com>
2017-08-08 22:24:16 -0600

    Comment and test geocontroller

--- src/main/java/controllers/GeoController.java
+++ src/main/java/controllers/GeoController.java
@@ -2,43 +2,63 @@ package controllers;
 
 /**
  * Created by mackenzie on 08/08/17.
+ *
+ * This class is a rudimentary implementation of a geometric bounding box.
+ * In the future this would updated to use the Haversine formula, especially
+ * if the requirement of bounding 10 miles is enlarged
+ *
+ * If the database is updated from SQLITE to a more powerful database (eg PostgreSQL)
+ * this class will become largely irrelevant as your bounding box can be calculated directly
+ * in the query.
  */
 public class GeoController {
-    //Semi-axes of WGS-84 geoidal reference
-    private double WGS84_a = 6378137.0;  // Major semiaxis in meters
-    private double WGS84_b = 6356752.3;  // Minor semiaxis in meters
-
+    /**
+     * Constructor for GeoController
+     */
     public GeoController() {
     }
 
+    /**
+     * Helper class for the GeoController
+     */
     private class GeoHelper{
 
         public GeoHelper() {
         }
 
+        /**
+         * Converts degrees to radians
+         *
+         * @param degrees degrees
+         * @return radians
+         */
         public double deg2Rad(double degrees){
             return Math.PI*degrees/180*0;
         }
 
-        public double rad2Deg(double radians){
-            return (180.0 * radians) / Math.PI;
-        }
-
-        public double WGS84EarthRad(double lat){
-            // http://en.wikipedia.org/wiki/Earth_radius
-            double An = WGS84_a*WGS84_a * Math.cos(lat);
-            double Bn = WGS84_b*WGS84_b * Math.sin(lat);
-            double Ad = WGS84_a * Math.cos(lat);
-            double Bd = WGS84_b * Math.sin(lat);
-            return Math.sqrt( (An*An + Bn*Bn)/(Ad*Ad + Bd*Bd) );
-        }
     }
 
-
-
+    /**
+     * Generates an array of doubles to serve as a bounding box for the Inputted point
+     *
+     * The current method provides a suitably accurate bounding box for small distances
+     * (less than 15mi). It computes the approximate miles per degree of latitude and
+     * longitude and uses that to calculate the change in latitude or longitude by
+     * dividing the distance by the degree approximation.
+     *
+     * @param latInDegrees Degrees of latitude
+     * @param lonInDegrees Degrees of longitude
+     * @param distance distance in miles that you want to bound. NOTE: you will require a
+     *                 more accurate formula if you want to bound large areas
+     * @return double array of the following format = [minLat, maxLat, minLon, maxLon]
+     */
     public double[ ] boundingBox(double latInDegrees, double lonInDegrees, double distance){
         double result[] = new double[4];
         GeoHelper gh = new GeoHelper();
+        if(distance >= 15) throw new IllegalArgumentException();
+        if(latInDegrees < -90 || latInDegrees > 90) throw new IllegalArgumentException();
+        if(lonInDegrees < -180 || lonInDegrees > 180) throw new IllegalArgumentException();
+
         double latApproximation = 69; // 69 miles per degree
         double lonApproximation = Math.cos(gh.deg2Rad(latInDegrees))*latApproximation;
 
---
+++ src/test/java/controllers/GeoControllerTest.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import org.junit.Test;
+
+/**
+ * Created by mackenzie on 08/08/17.
+ */
+public class GeoControllerTest {
+    private double validLat = 37.809052;
+    private double validLon = -122.483365;
+    private double distance = 10.0; //miles as per requirement
+
+    @Test
+    public void boundingBox() throws Exception {
+        GeoController gc  = new GeoController();
+        double[] bounds = gc.boundingBox(validLat, validLon, distance);
+        //TODO validate output better
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void boundingBox_badLat() throws Exception{
+         double[] bounds = new GeoController().boundingBox(-91.2, validLon, distance);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void boundingBox_badLon() throws Exception{
+         double[] bounds = new GeoController().boundingBox(validLat, 181.2, distance);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void boundingBox_badDistance() throws Exception{
+         double[] bounds = new GeoController().boundingBox(validLat, validLon, 20.0);
+    }
+}
